0768fdeaec81c7b405cc5b9de0393cbea7b6af43
ManuelEusebio <154792550+CarlosManuelEusebioAlejo@users.noreply.github.com>
2024-02-27 07:24:12 -0600

    Add files via upload

---
+++ proyecto_integrador_2-feature-Eusebio/InicioSesion/bienvenida.php
@@ -0,0 +1,31 @@
+<?php
+/*
+    session_start();
+
+    if(!isset($_SESSION['usuario']))
+    {
+        echo '
+            <script>
+                alert("Por favor debes iniciar sesion");
+                window.location = "index.php";
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        bienvenido a tu pagina
+    </h1>
+</body>
+</html>
