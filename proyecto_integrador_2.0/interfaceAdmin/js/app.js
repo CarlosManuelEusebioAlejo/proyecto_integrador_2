cea2f940d99f490d6c9d14aeafb8beca50c662a5
Angel Alvarez <joseangelalvarezx@gmail.com>
2024-03-16 04:12:00 -0600

    responsive

---
+++ proyecto_integrador_2.0/interfaceAdmin/js/app.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var botonToggle = document.getElementById('toggleButton');
+//     var rectangulo = document.getElementById('rectangulo');
+//     var contenedor = document.getElementById('contenedor');
+//     var visible = false;
+
+//     botonToggle.addEventListener('click', function(){
+//         if (visible) {
+//             contenedor.style.maxHeight = '0px';
+//             visible = false;
+//         }
+//         else{
+//             contenedor.style.maxHeight = rectangulo.scrollHeight + 'px';
+//             visible = true;
+//         }
+//     });
+// });
